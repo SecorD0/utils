09df012d4b682e4fefd42707d6b817bbb848ff8f
SecorD0 <vovan6573@gmail.com>
2021-07-28 16:48:20 +0300

    Installs GO v1.16.5

--- golang_installer.sh
+++ golang_installer.sh
@@ -4,8 +4,8 @@ if ! go version | grep -q $version; then
 	sudo apt install tar -y
 	cd $HOME
 	wget "https://golang.org/dl/go$version.linux-amd64.tar.gz"
-	rm -rf /usr/local/go
-	tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
+	sudo rm -rf /usr/local/go
+	sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
 	rm go1.16.5.linux-amd64.tar.gz
 	echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 	source ~/.bash_profile
