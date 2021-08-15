dad258b77cf160fc81b0341c87b35ef47b9cb627
SecorD0 <vovan6573@gmail.com>
2021-08-16 02:54:54 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,7 +1,7 @@
 source ~/.bash_profile
 if ! cat ~/.bash_profile | grep -q "$1"; then
 	echo "export $1=$2" >> ~/.bash_profile
-elif ! cat ~/.bash_profile | grep -q "$2"; then
+elif ! cat ~/.bash_profile | grep -q "$1=\"$2\""; then
 	sed -i "s/export $1*=.*/export $1=\"$2\"/" ~/.bash_profile
 fi
 source ~/.bash_profile
