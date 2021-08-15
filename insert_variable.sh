c53625df1a7522992554d2259e45e39f9fd0448c
SecorD0 <vovan6573@gmail.com>
2021-08-16 02:32:12 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,9 +1,9 @@
 source ~/.bash_profile
 if ! cat ~/.bash_profile | grep -q "$1"; then
-echo "1"
-echo "export $1=$2" >> ~/.bash_profile && source ~/.bash_profile
+	echo "1"
+	echo "export $1=$2" >> ~/.bash_profile && source ~/.bash_profile
 elif ! cat ~/.bash_profile | grep -q "$2"; then
-echo "2"
-sed -i "s/export $1*=.*/export massa_wallet_address=$2/" ~/.bash_profile
+	echo "2"
+	sed -i "s/export $1*=.*/export massa_wallet_address=$2/" ~/.bash_profile
 fi
 source ~/.bash_profile
