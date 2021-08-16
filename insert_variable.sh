931248a2c6f7508a4fba3c1fdd67f935a0460bad
SecorD0 <vovan6573@gmail.com>
2021-08-17 01:31:01 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,7 +1,12 @@
+if [ -n "$3" ]; then
+	var_or_com="alias"
+else
+	var_or_com="export"
+fi
 . ~/.bash_profile
 if ! cat ~/.bash_profile | grep -q "$1"; then
-	echo "export $1=$2" >> ~/.bash_profile
+	echo "$var_or_com $1=\"$2\"" >> ~/.bash_profile
 elif ! cat ~/.bash_profile | grep -q "$1=\"$2\""; then
-	sed -i "s/export $1*=.*/export $1=\"$2\"/" ~/.bash_profile
+	sed -i "s/$var_or_com $1*=.*/$var_or_com $1=\"$2\"/" ~/.bash_profile
 fi
 . ~/.bash_profile
