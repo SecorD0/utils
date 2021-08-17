4c0bc669a6c507d8ddef09b6d89d7655f7af7b64
SecorD0 <vovan6573@gmail.com>
2021-08-17 23:34:07 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,7 +1,7 @@
 # $1 - variable name
 # $2 - variable value
 # $3 - is it a command? (true, false)
-# $4 - replace it to $1
+# $4 - replase it to $1
 #!/bin/bash
 if [ "$2" = "" ]; then
 	read -p $'Enter the value: \e[40m\e[92m' value
@@ -19,10 +19,14 @@ if [ -n "$4" ]; then
 . ~/.bash_profile
 fi
 if ! cat ~/.bash_profile | grep -q "$1"; then
+	echo 1
 	echo "$is_command $1=\"$value\"" >> ~/.bash_profile
 elif ! cat ~/.bash_profile | grep -q "$1=\"$value\""; then
-	sed -i "s/^$iscommand $1*=.*/$iscommand $1=\"$value\"/" ~/.bash_profile
-elif cat ~/.bash_profile | grep -q "$1=\"$value\""; then
+	echo 2
+	sed -i "s/^.*$1*=.*/$is_command $1=\"$value\"/" ~/.bash_profile
+fi
+if cat ~/.bash_profile | grep -q "$1=\"$value\""; then
+	echo 3
 	sed -i "s/^.*$1*=.*/$is_command $1=\"$value\"/" ~/.bash_profile
 fi
 . ~/.bash_profile
