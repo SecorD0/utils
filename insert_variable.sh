1917af12188c8ece5e57958b57e8ae9985a9ea76
SecorD0 <vovan6573@gmail.com>
2021-08-17 13:42:46 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,9 +1,16 @@
+# $1 - variable name
+# $2 - variable value
+# $3 - is it a command? (true, false)
+# $4 - replase it to $1
 if [ -n "$3" ]; then
 	is_command="alias"
 else
 	is_command="export"
 fi
+if [ -n "$4" ]; then
+	sed -i "s/$4/$1/" ~/.bash_profile
 . ~/.bash_profile
+fi
 if ! cat ~/.bash_profile | grep -q "$1"; then
 	echo "$is_command $1=\"$2\"" >> ~/.bash_profile
 elif ! cat ~/.bash_profile | grep -q "$1=\"$2\""; then
