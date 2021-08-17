872c5ce7b76030eb7d724767af3da916871bc306
SecorD0 <vovan6573@gmail.com>
2021-08-17 23:06:11 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -3,7 +3,13 @@
 # $3 - is it a command? (true, false)
 # $4 - replase it to $1
 #!/bin/bash
-if $3; then
+if [ "$2" = "" ]; then
+	read -p $'Enter the value: \e[40m\e[92m' value
+	echo -n -e '\e[0m'
+else
+	value=$2
+fi
+if [ "$3" = "true" ]; then
 	is_command="alias"
 else
 	is_command="export"
@@ -13,8 +19,10 @@ if [ -n "$4" ]; then
 . ~/.bash_profile
 fi
 if ! cat ~/.bash_profile | grep -q "$1"; then
-	echo "$is_command $1=\"$2\"" >> ~/.bash_profile
-elif ! cat ~/.bash_profile | grep -q "$1=\"$2\""; then
-	sed -i "s/$is_command $1*=.*/$is_command $1=\"$2\"/" ~/.bash_profile
+	echo "$is_command $1=\"$value\"" >> ~/.bash_profile
+elif ! cat ~/.bash_profile | grep -q "$1=\"$value\""; then
+	sed -i "s/^$iscommand $1*=.*/$iscommand $1=\"$value\"/" ~/.bash_profile
+elif cat ~/.bash_profile | grep -q "$1=\"$value\""; then
+	sed -i "s/^.*$1*=.*/$is_command $1=\"$value\"/" ~/.bash_profile
 fi
 . ~/.bash_profile
