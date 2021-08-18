e4983d7bad7bff99f576a8cf5345e72d59f0e1ac
SecorD0 <vovan6573@gmail.com>
2021-08-18 19:19:02 +0300

    Inserts variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -3,6 +3,7 @@
 # $3 - is it a command? (true, false)
 # $4 - replase it to $1
 #!/bin/bash
+. ~/.bash_profile
 if [ "$2" = "" ]; then
 	read -p $'\e[40m\e[92mEnter the value:\e[0m ' value
 else
@@ -14,15 +15,14 @@ else
 	is_command="export"
 fi
 if [ -n "$4" ]; then
-	sed -i "s/$4/$1/" ~/.bash_profile
-. ~/.bash_profile
+	sed -i "s%$4%$1%" ~/.bash_profile
 fi
 if ! cat ~/.bash_profile | grep -q "$1"; then
 	echo "$is_command $1=\"$value\"" >> ~/.bash_profile
 elif ! cat ~/.bash_profile | grep -q "$1=\"$value\""; then
-	sed -i "s/^.*$1*=.*/$is_command $1=\"$value\"/" ~/.bash_profile
+	sed -i "s%^.*$1*=.*%$is_command $1=\"$value\"%" ~/.bash_profile
 fi
 if cat ~/.bash_profile | grep -q "$1=\"$value\""; then
-	sed -i "s/^.*$1*=.*/$is_command $1=\"$value\"/" ~/.bash_profile
+	sed -i "s%^.*$1*=.*%$is_command $1=\"$value\"%" ~/.bash_profile
 fi
 . ~/.bash_profile
