69ea9dd58cd5f714a5ef981d67edb282b487535e
SecorD0 <vovan6573@gmail.com>
2021-08-24 19:39:11 +0300

    Installs NVM

--- nvm_installer.sh
+++ nvm_installer.sh
@@ -1,14 +1,14 @@
 # $1 - NVM version
 #!/bin/bash
 if [ -n "$1" ]; then
-	version=$1
+	nvm_version=$1
 else
-	version="0.38.0"
+	nvm_version="0.38.0"
 fi
-if ! nvm --version | grep -q $version; then
+if ! nvm --version | grep -q $nvm_version; then
 	sudo apt install wget -y
 	cd $HOME
-	. <(wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v$version/install.sh")
+	. <(wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v$nvm_version/install.sh")
 	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 	. ~/.bashrc
