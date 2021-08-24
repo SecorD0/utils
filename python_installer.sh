ead54bcb88ab4351030bf0438a8e787795f29b8a
SecorD0 <vovan6573@gmail.com>
2021-08-24 19:39:53 +0300

    Installs Python

--- python_installer.sh
+++ python_installer.sh
@@ -1,17 +1,17 @@
 # $1 - Python version
 #!/bin/bash
 if [ -n "$1" ]; then
-	version=$1
+	python_version=$1
 else
-	version="3.9.6"
+	python_version="3.9.6"
 fi
-if ! python3 --version | grep -q $version; then
+if ! python3 --version | grep -q $python_version; then
 	sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
-	wget "https://www.python.org/ftp/python/$version/Python-$version.tgz"
+	wget "https://www.python.org/ftp/python/$python_version/Python-$python_version.tgz"
 	sudo rm -rf /usr/local/python
-	sudo tar -C /usr/local -xzf "Python-$version.tgz"
-	rm "Python-$version.tgz"
-	sudo mv "/usr/local/Python-$version" /usr/local/python
+	sudo tar -C /usr/local -xzf "Python-$python_version.tgz"
+	rm "Python-$python_version.tgz"
+	sudo mv "/usr/local/Python-$python_version" /usr/local/python
 	cd /usr/local/python
 	./configure --enable-optimizations
 	make
