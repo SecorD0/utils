0417069bf934c21a142010535bb3c4c41925c26f
SecorD0 <vovan6573@gmail.com>
2021-08-24 19:41:00 +0300

    Installs Node.js

--- nodejs_installer.sh
+++ nodejs_installer.sh
@@ -1,13 +1,13 @@
 # $1 - Node.js version
 #!/bin/bash
 if [ -n "$1" ]; then
-	node_version=$1
+	nodejs_version=$1
 else
-	node_version="14.17.5"
+	nodejs_version="14.17.5"
 fi
-if ! node --version | grep -q $node_version; then
+if ! node --version | grep -q $nodejs_version; then
 	sudo apt install tar wget -y
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/nvm_installer.sh)
-	nvm install $node_version
-	nvm use $node_version
+	nvm install $nodejs_version
+	nvm use $nodejs_version
 fi
