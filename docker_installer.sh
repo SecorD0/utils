d48219e5f2eb73bdd628bfd7271442b35facd172
SecorD0 <vovan6573@gmail.com>
2021-08-25 01:13:47 +0300

    Installs Docker

--- docker_installer.sh
+++ docker_installer.sh
@@ -5,8 +5,9 @@ if ! docker --version; then
 	sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt install docker-ce -y
-	sudo apt install docker-ce-cli containerd.io -y
-	version=$(apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)")
-	sudo apt install docker-ce="$version" docker-ce-cli="$version" containerd.io -y
+	sudo apt update
+	sudo apt install docker-ce docker-ce-cli containerd.io -y
+	docker_version=$(apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)")
+	sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+  echo
 fi
