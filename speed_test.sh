73b32e0c0fe7f77023fc865bbc0daa1435380d89
SecorD0 <vovan6573@gmail.com>
2021-09-10 14:43:16 +0300

    Measures the command(s) execution speed

--- speed_test.sh
+++ speed_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# $1 - any command
+# $1 - any command(s) (string)
 sudo apt install jq -y
 start=$(date +%s.%N)
-$1
-. ~/.bash_profile
-echo -e "The operation lasted \e[40m\e[92m$(jq -n "$(date +%s.%N)-$start")\e[0m seconds"
+for command in "$@"; do
+	$command | sed -e "s%\"%%g; s%'%%g"
+	. ~/.bash_profile
+	echo -e "The operation lasted \e[40m\e[92m$(jq -n "$(date +%s.%N)-$start")\e[0m seconds"
+done
