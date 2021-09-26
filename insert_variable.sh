34eb29cc62f17265ee435e4b34180e39a2c59a73
SecorD0 <vovan6573@gmail.com>
2021-09-26 13:43:32 +0300

    Inserts/deletes variable

--- insert_variable.sh
+++ insert_variable.sh
@@ -1,29 +1,94 @@
 #!/bin/bash
-# $1 - variable name
-# $2 - variable value
-# $3 - is it a command? (true, false)
-# $4 - replase it to $1
-touch ~/.bash_profile
-. ~/.bash_profile
-if [ "$2" = "" ]; then
-	read -p $'\e[40m\e[92mEnter the value:\e[0m ' value
+# Default variables
+name=""
+value=""
+type="export"
+rename=""
+delete="false"
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script inserts variable or alias to ${C_LGn}$HOME/.bash_profile${RES}"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help           show the help page"
+		echo -e "  -n, --name NAME      variable or alias NAME"
+		echo -e "  -v, --value VALUE    variable or alias VALUE (leave blank for manual input)"
+		echo -e "  -a, --alias          inserting an alias"
+		echo -e "  -r, --rename NAME_2  rename NAME_2 to NAME"
+		echo -e "  -d, --delete         delete variable or alias by NAME"
+		echo
+		echo -e "${C_LGn}Useful URLs${RES}:"
+		echo -e "https://github.com/SecorD0/utils/blob/main/insert_variable.sh - script URL"
+		echo -e "https://t.me/letskynode — node Community"
+		echo
+		return 0
+		;;
+	-n*|--name*)
+		if ! grep -q "=" <<< "$1"; then shift; fi
+		name=`option_value "$1"`
+		shift
+		;;
+	-v*|--value*)
+		if ! grep -q "=" <<< "$1"; then shift; fi
+		value=`option_value "$1"`
+		shift
+		;;
+	-a|--alias)
+		type="alias"
+		shift
+		;;
+	-r*|--rename*)
+		if ! grep -q "=" <<< "$1"; then shift; fi
+		rename=`option_value "$1"`
+		shift
+		;;
+	-d|--delete)
+		delete="true"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+# Actions
+touch $HOME/.bash_profile
+. $HOME/.bash_profile
+if [ ! -n "$name" ]; then
+	printf_n "${C_R}You didn't specify a name via${RES} -n ${C_R}option!${RES}"
+	return 1
+fi
+if [ "${delete}" = "true" ]; then
+	sed -i "/ ${name}=/d" $HOME/.bash_profile
+	unset "$name"
+	unalias "$name" 2>/dev/null
 else
-	value=$2
+	if [ ! -n "${value}" ]; then
+		printf "${C_LGn}Enter the value:${RES} "
+		read value
+	fi
+	if [ -n "${rename}" ]; then
+		sed -i "s%${rename}%${name}%" $HOME/.bash_profile
+		unset "$rename"
+		unalias "$rename" 2>/dev/null
+	fi
+	if ! cat $HOME/.bash_profile | grep -q " ${name}="; then
+		echo "${type} ${name}=\"${value}\"" >> $HOME/.bash_profile
+	elif ! cat $HOME/.bash_profile | grep -q "${name}=\"${value}\""; then
+		sed -i "s%^.*${name}*=.*%${type} ${name}=\"${value}\"%" $HOME/.bash_profile
+	fi
+	if cat $HOME/.bash_profile | grep -q "${name}=\"${value}\""; then
+		sed -i "s%^.*${name}*=.*%${type} ${name}=\"${value}\"%" $HOME/.bash_profile
+	fi
 fi
-if [ "$3" = "true" ]; then
-	is_command="alias"
-else
-	is_command="export"
-fi
-if [ -n "$4" ]; then
-	sed -i "s%$4%$1%" ~/.bash_profile
-fi
-if ! cat ~/.bash_profile | grep -q "$1"; then
-	echo "$is_command $1=\"$value\"" >> ~/.bash_profile
-elif ! cat ~/.bash_profile | grep -q "$1=\"$value\""; then
-	sed -i "s%^.*$1*=.*%$is_command $1=\"$value\"%" ~/.bash_profile
-fi
-if cat ~/.bash_profile | grep -q "$1=\"$value\""; then
-	sed -i "s%^.*$1*=.*%$is_command $1=\"$value\"%" ~/.bash_profile
-fi
-. ~/.bash_profile
+. $HOME/.bash_profile
