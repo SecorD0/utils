3937a51e8cc1847e1ae16d0f5631b9d37185329b
SecorD0 <vovan6573@gmail.com>
2021-09-26 13:13:18 +0300

    Installs/uninstalls Node.js

--- nodejs_installer.sh
+++ nodejs_installer.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
-# $1 - Node.js version
-if [ -n "$1" ]; then
-	nodejs_version=$1
-else
-	nodejs_version="14.17.5"
-fi
-if ! node --version | grep -q $nodejs_version; then
-	sudo apt install tar wget -y
+# Default variables
+nodejs_version="14.17.5"
+uninstall="false"
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script installs or uninstalls Node.js"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help             show the help page"
+		echo -e "  -v, --version VERSION  Node.js VERSION to install (default is ${C_LGn}${nodejs_version}${RES})"
+		echo -e "  -u, --uninstall        uninstall Node.js"
+		echo
+		echo -e "${C_LGn}Useful URLs${RES}:"
+		echo -e "https://github.com/SecorD0/utils/blob/main/nodejs_installer.sh - script URL"
+		echo -e "https://t.me/letskynode — node Community"
+		echo
+		return 0
+		;;
+	-v*|--version*)
+		if ! grep -q "=" <<< "$1"; then shift; fi
+		nodejs_version=`option_value "$1"`
+		shift
+		;;
+	-u|--uninstall)
+		uninstall="true"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+# Actions
+if [ "$uninstall" = "true" ]; then
+	echo -e "${C_LGn}Uninstalling Node.js...${RES}"
+	nvm deactivate
+	nvm uninstall $nodejs_version
+elif ! node --version | grep -q $nodejs_version; then
+	echo -e "${C_LGn}Installing Node.js...${RES}"
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/nvm_installer.sh)
 	nvm install $nodejs_version
 	nvm use $nodejs_version
 fi
+echo -e "${C_LGn}Done!${RES}"
