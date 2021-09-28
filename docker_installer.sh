e04d30aab9e1c2fe4fc38f7cdd904059c5e8bbf2
SecorD0 <vovan6573@gmail.com>
2021-09-29 00:14:22 +0300

    Installs/uninstalls Docker and Dive

--- docker_installer.sh
+++ docker_installer.sh
@@ -58,7 +58,7 @@ else
 		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
 		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
 	fi
-	if ! dpkg -s dive | grep -q installed; then
+	if ! dpkg -s dive | grep -q "ok installed"; then
 		echo -e "${C_LGn}Dive installing...${RES}"
 		wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
 		sudo apt install ./dive_0.9.2_linux_amd64.deb
