382e2b27c4ea9b69d3f07734b232211e3efaf8b7
SecorD0 <vovan6573@gmail.com>
2021-10-09 01:10:26 +0300

    Installs/uninstalls NVM

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -47,7 +47,7 @@ if [ "$uninstall" = "true" ]; then
 	rm -rf $NVM_DIR
 	unset nvm
 elif ! nvm --version | grep -q $nvm_version; then
-	echo -e "${C_LGn}Installing NVM...${RES}"
+	echo -e "${C_LGn}NVM installation...${RES}"
 	sudo apt install wget -y
 	cd $HOME
 	. <(wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v${nvm_version}/install.sh")
