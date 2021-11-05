abc1ae284d2fbe9ab7a507faf326e3bdce23ef09
SecorD0 <vovan6573@gmail.com>
2021-11-06 00:11:44 +0300

    Installs Python

--- installers/python.sh
+++ installers/python.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Default variables
+function="install"
 python_version="3.9.7"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -35,20 +37,26 @@ while test $# -gt 0; do
 		;;
 	esac
 done
-# Actions
-if ! python3 --version | grep -q "3.8.10"; then
+
+# Functions
+install() {
 	echo -e "${C_LGn}Python installation...${RES}"
-	sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
-	wget "https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz"
-	sudo rm -rf /usr/local/python
-	sudo tar -C /usr/local -xzf "Python-${python_version}.tgz"
-	rm "Python-${python_version}.tgz"
-	sudo mv "/usr/local/Python-${python_version}" /usr/local/python
-	cd /usr/local/python
-	./configure --enable-optimizations
-	make
-	sudo make altinstall
-	sudo apt install python3-pip -y 
-	cd
-fi
+	if ! python3 --version | grep -q "3.8.10"; then
+		sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+		wget "https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz"
+		sudo rm -rf /usr/local/python
+		sudo tar -C /usr/local -xzf "Python-${python_version}.tgz"
+		rm "Python-${python_version}.tgz"
+		sudo mv "/usr/local/Python-${python_version}" /usr/local/python
+		cd /usr/local/python
+		./configure --enable-optimizations
+		make
+		sudo make altinstall
+		sudo apt install python3-pip -y 
+		cd
+	fi
+}
+
+# Actions
+$function
 echo -e "${C_LGn}Done!${RES}"
