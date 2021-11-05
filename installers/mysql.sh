aa200cda74879147f44e78ae09fdc5ae02ddcdc7
SecorD0 <vovan6573@gmail.com>
2021-11-05 22:31:49 +0300

    Installs/uninstalls MySQL

--- installers/mysql.sh
+++ installers/mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Default variables
-uninstall="false"
+function="install"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -24,7 +25,7 @@ while test $# -gt 0; do
 		return 0 2>/dev/null; exit 0
 		;;
 	-u|--uninstall)
-		uninstall="true"
+		function="uninstall"
 		shift
 		;;
 	*|--)
@@ -32,9 +33,20 @@ while test $# -gt 0; do
 		;;
 	esac
 done
-# Actions
-if [ "$uninstall" = "true" ]; then
-	echo -e "${C_LGn}Uninstalling MySQL...${RES}"
+
+# Functions
+install() {
+	echo -e "${C_LGn}MySQL installation...${RES}"
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt install mysql-server mysql-client -y
+	echo -e "
+Execute it to continue the installation:
+${C_LGn}sudo mysql_secure_installation${RES}
+"
+}
+uninstall() {
+	echo -e "${C_LGn}MySQL uninstalling...${RES}"
 	sudo systemctl stop mysql
 	sudo killall -KILL mysql mysqld_safe mysqld
 	sudo apt -y purge mysql-server mysql-client
@@ -45,13 +57,7 @@ if [ "$uninstall" = "true" ]; then
 	rm -rf /etc/apparmor.d/abstractions/mysql /etc/apparmor.d/cache/usr.sbin.mysqld /etc/mysql /var/lib/mysql /var/log/mysql* /var/log/upstart/mysql.log* /var/run/mysqld
 	sudo find / -name .mysql_history -delete
 	echo -e "${C_LGn}Done!${RES}"
-else
-	echo -e "${C_LGn}MySQL installation...${RES}"
-	sudo apt update
-	sudo apt upgrade -y
-	sudo apt install mysql-server mysql-client -y
-	echo -e "
-Execute it to continue the installation:
-${C_LGn}sudo mysql_secure_installation${RES}
-"
-fi
+}
+
+# Actions
+$function
