bc77140ce36229cf13837e5b362dd776a54c335a
SecorD0 <vovan6573@gmail.com>
2021-11-22 21:31:45 +0300

    Opens ports

--- miscellaneous/ports_opening.sh
+++ miscellaneous/ports_opening.sh
@@ -27,22 +27,29 @@ while test $# -gt 0; do
 		;;
 	esac
 done
-# Actions
-echo -e "${C_LGn}Port(s) opening...${RES}"
-if sudo ufw status | grep -q "Status: active"; then
-	sudo ufw allow 22
-	for port in "$@"; do
-		sudo ufw allow "$port"
-	done
-else
-	if ! dpkg --get-selections | grep -qP "(?<=iptables-persistent)([^de]+)(?=install)"; then
-		echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-		echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-		sudo apt install iptables-persistent -y
+
+# Functions
+main() {
+	echo -e "${C_LGn}Port(s) opening...${RES}"
+	if sudo ufw status | grep -q "Status: active"; then
+		sudo ufw allow 22
+		for open_this_port in "$@"; do
+			sudo ufw allow "$open_this_port"
+		done
+	else
+		if ! dpkg --get-selections | grep -qP "(?<=iptables-persistent)([^de]+)(?=install)"; then
+			echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+			echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+			sudo apt install iptables-persistent -y
+		fi
+		for open_this_port in "$@"; do
+			sudo iptables -I INPUT -p tcp --dport "$open_this_port" -j ACCEPT
+		done
+		sudo netfilter-persistent save
 	fi
-	for port in "$@"; do
-		sudo iptables -I INPUT -p tcp --dport "$port" -j ACCEPT
-	done
-	sudo netfilter-persistent save
-fi
-echo -e "${C_LGn}Done!${RES}"
+	unset open_this_port
+	echo -e "${C_LGn}Done!${RES}"
+}
+
+# Actions
+main
