664b62a3159e4690770bdd083576e6932c6bedb3
SecorD0 <vovan6573@gmail.com>
2021-12-09 01:52:27 +0300

    Installs/uninstalls Docker and Dive

--- installers/docker.sh
+++ installers/docker.sh
@@ -40,6 +40,7 @@ install() {
 	cd
 	if ! docker --version; then
 		sudo apt update
+		sudo apt upgrade -y
 		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
