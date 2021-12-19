ad8758c25a710e8c133b1e28f10f1a90cad09962
SecorD0 <vovan6573@gmail.com>
2021-12-20 01:43:50 +0300

    Inserts/deletes variable

--- miscellaneous/insert_variable.sh
+++ miscellaneous/insert_variable.sh
@@ -4,7 +4,8 @@ name=""
 value=""
 type="export"
 rename=""
-delete="false"
+delete_type="none"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -18,12 +19,13 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
 		echo
 		echo -e "${C_LGn}Options${RES}:"
-		echo -e "  -h, --help           show the help page"
-		echo -e "  -n, --name NAME      variable or alias NAME (mandatory)"
-		echo -e "  -v, --value VALUE    variable or alias VALUE (leave blank for manual input)"
-		echo -e "  -a, --alias          inserting an alias"
-		echo -e "  -r, --rename NAME_2  rename NAME_2 to NAME"
-		echo -e "  -d, --delete         delete variable or alias by NAME"
+		echo -e "  -h,  --help           show the help page"
+		echo -e "  -n,  --name NAME      variable or alias NAME (mandatory)"
+		echo -e "  -v,  --value VALUE    variable or alias VALUE (leave blank for manual input)"
+		echo -e "  -a,  --alias          inserting an alias"
+		echo -e "  -r,  --rename NAME_2  rename NAME_2 to NAME"
+		echo -e "  -d,  --delete         delete the first occurrence of a variable or alias by NAME"
+		echo -e "  -da, --delete-all     delete all occurrences of a variable or alias by NAME"
 		echo
 		echo -e "You can use either \"=\" or \" \" as an option and value ${C_LGn}delimiter${RES}"
 		echo
@@ -53,7 +55,11 @@ while test $# -gt 0; do
 		shift
 		;;
 	-d|--delete)
-		delete="true"
+		delete="delete"
+		shift
+		;;
+	-d|--delete)
+		delete="delete_all"
 		shift
 		;;
 	*|--)
@@ -61,28 +67,37 @@ while test $# -gt 0; do
 		;;
 	esac
 done
+
 # Functions
 printf_n(){ printf "$1\n" "${@:2}"; }
+
 # Actions
 touch $HOME/.bash_profile
 . $HOME/.bash_profile
 if [ ! -n "$name" ]; then
 	printf_n "${C_R}You didn't specify a name via${RES} -n ${C_R}option!${RES}"
-	return 1 2>/dev/null; exit 0
+	return 1 2>/dev/null; exit 1
 fi
-if [ "${delete}" = "true" ]; then
-	sed -i "/ ${name}=/d" $HOME/.bash_profile
+if [ "$delete_type" != "none" ]; then
+	if [ "$delete_type" = "delete" ]; then
+		sed -i '0,/ ${name}=/{/ ${name}=/d;}' $HOME/.bash_profile
+	elif [ "$delete_type" = "delete_all" ]; then
+		sed -i "/ ${name}=/d" $HOME/.bash_profile
+	fi
 	unset "$name"
 	unalias "$name" 2>/dev/null
 else
-	if [ ! -n "${value}" ]; then
-		printf "${C_LGn}Enter the value:${RES} "
-		read value
-	fi
-	if [ -n "${rename}" ]; then
+	if [ -n "$rename" ]; then
 		sed -i "s%${rename}%${name}%" $HOME/.bash_profile
 		unset "$rename"
 		unalias "$rename" 2>/dev/null
+		if [ ! -n "$value" ]; then
+			return 0 2>/dev/null; exit 0
+		fi
+	fi
+	if [ ! -n "$value" ]; then
+		printf "${C_LGn}Enter the value:${RES} "
+		read -r value
 	fi
 	if ! cat $HOME/.bash_profile | grep -q " ${name}="; then
 		echo "${type} ${name}=\"${value}\"" >> $HOME/.bash_profile
@@ -94,4 +109,6 @@ else
 		sed -i "s%^.*${name}*=.*%${type} ${name}=\"${value}\"%" $HOME/.bash_profile
 	fi
 fi
+sed -i '/^$/d' $HOME/.bash_profile
+echo >> $HOME/.bash_profile
 . $HOME/.bash_profile
