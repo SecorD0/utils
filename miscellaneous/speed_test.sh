ed42fef571fd6586043d86f5dfd82583253d077f
SecorD0 <vovan6573@gmail.com>
2022-01-05 20:57:58 +0300

    Measures command execution speed

--- miscellaneous/speed_test.sh
+++ miscellaneous/speed_test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Default variables
+total="false"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -12,7 +15,7 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES} ${C_LGn}[ARGUMENTS]${RES}"
 		echo
 		echo -e "${C_LGn}Options${RES}:"
-		echo -e "  -h, --help  show the help page"
+		echo -e "  -h, --help   show the help page"
 		echo
 		echo -e "${C_LGn}Arguments${RES} - any commands as strings separated by spaces. For example: \"df\" 'ls -la'"
 		echo
@@ -20,18 +23,41 @@ while test $# -gt 0; do
 		echo -e "https://github.com/SecorD0/utils/blob/main/miscellaneous/speed_test.sh - script URL"
 		echo -e "https://t.me/letskynode — node Community"
 		echo
-		return 0
+		return 0 2>/dev/null; exit 0
+		;;
+	-t|--total)
+		total="true"
+		shift
 		;;
 	*|--)
 		break
 		;;
 	esac
 done
+
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+main() {
+	sudo apt install bc -y &>/dev/null
+	local commands_l=()
+	local runtime_l=()
+	for command in "$@"; do
+		commands_l+=("$command")
+	done
+	for command in "$@"; do
+		local command_start=$(date +%s.%N)
+		eval $command
+		. $HOME/.bash_profile
+		runtime_l+=(`bc <<< "$(date +%s.%N)-$command_start"`)
+	done
+	printf_n "\n\n${C_LGn}Time taken (s)${RES}\t|  Command\n------------------------------------"
+	local sum=0.00
+	for ((i = 0; i < "${#commands_l[@]}"; i++)); do
+		printf_n "${C_LGn}%f${RES}\t|  ${commands_l[$i]}" "${runtime_l[$i]}"
+		local sum=`bc <<< "$sum+${runtime_l[$i]}"`
+	done
+	printf_n "${C_LGn}%f${RES}\t|  Total" "$sum"
+}
+
 # Actions
-sudo apt install jq -y &>/dev/null
-start=$(date +%s.%N)
-for command in "$@"; do
-	eval $command
-	. $HOME/.bash_profile
-	echo -e "The operation lasted ${C_LGn}`jq -n "$(date +%s.%N)-$start"`${RES} seconds\n"
-done
+main "$@"
