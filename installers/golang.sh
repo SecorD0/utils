0c8767c1eb2775a3abc8b506aa1d4804e79f662f
SecorD0 <vovan6573@gmail.com>
2022-07-21 15:21:35 +0300

    Installs/uninstalls GO

--- installers/golang.sh
+++ installers/golang.sh
@@ -16,9 +16,9 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
 		echo
 		echo -e "${C_LGn}Options${RES}:"
-		echo -e "  -h, --help             show the help page"
-		echo -e "  -v, --version VERSION  GO VERSION to install (default is ${C_LGn}${go_version}${RES})"
-		echo -e "  -u, --uninstall        uninstall GO"
+		echo -e "  -h,  --help             show the help page"
+		echo -e "  -v,  --version VERSION  GO VERSION to install (default is ${C_LGn}${go_version}${RES})"
+		echo -e "  -un, --uninstall        uninstall GO"
 		echo
 		echo -e "You can use either \"=\" or \" \" as an option and value ${C_LGn}delimiter${RES}"
 		echo
@@ -34,7 +34,7 @@ while test $# -gt 0; do
 		go_version=`option_value "$1"`
 		shift
 		;;
-	-u|--uninstall)
+	-u|-un|--uninstall)
 		function="uninstall"
 		shift
 		;;
